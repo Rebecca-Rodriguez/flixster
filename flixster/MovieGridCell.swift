9bee21407628c95ab6c697d61e112a45a1229d8d
RebeccaVelez <contact@rebeccavelez.dev>
2021-07-18 20:46:45 -0400

    Updates
    Updates

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Rebecca Velez on 7/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
